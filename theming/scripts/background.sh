4f2a8aa3096b18376d4b0047c9e4fc3f7f0daac5
Niek Janssen <niek.janssen01@gmail.com>
2019-11-29 23:24:10 +0100

    Maintenance of Fri 29 Nov 23:24:10 CET 2019

--- theming/scripts/background.sh
+++ theming/scripts/background.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wal -c -i /etc/theming/img/wallpaper.jpg -n
+wal -i /etc/theming/img/wallpaper.jpg -n
 convert /etc/theming/img/lockscreen.jpg /etc/theming/img/lockscreen.png
 feh --bg-fill /etc/theming/img/wallpaper.jpg
