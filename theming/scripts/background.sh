b4e47329f1dda7cd01b35f2458881a6f2dde53aa
Niek Janssen <niek.janssen01@gmail.com>
2020-03-09 11:43:52 +0100

    Maintenance of ma  9 mrt 2020 11:43:52 CET

--- theming/scripts/background.sh
+++ theming/scripts/background.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-wal -i /etc/theming/img/wallpaper.jpg -n
-convert /etc/theming/img/lockscreen.jpg /etc/theming/img/lockscreen.png
+cp /etc/theming/img/wallpaper.png /etc/theming/img/lockscreen.png
+convert /etc/theming/img/wallpaper.jpg /etc/theming/img/wallpaper.png
+wal -i /etc/theming/img/wallpaper.png -n
 feh --bg-fill /etc/theming/img/wallpaper.jpg
+
+/etc/theming/telegram/telegram-palette-gen --wal
