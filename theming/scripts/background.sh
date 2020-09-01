306fb0f083316615dbe74144634efc30c0c7b701
Niek Janssen <niek.janssen01@gmail.com>
2020-09-01 18:42:54 +0200

    update

--- theming/scripts/background.sh
+++ theming/scripts/background.sh
@@ -7,5 +7,6 @@ convert $DIR/img/wallpaper.jpg $DIR/img/wallpaper.png
 wal -c
 wal -i $DIR/img/wallpaper.png -n --saturate 0.8
 feh --bg-fill $DIR/img/wallpaper.jpg
+pywalfox update
 
 $DIR/telegram/telegram-palette-gen --wal
