5a089a301089e02236eeef211b9f3359dc804b36
Niek Janssen <niek.janssen01@gmail.com>
2020-09-01 12:06:17 +0200

    Maintenance of di  1 sep 2020 12:06:17 CEST

--- theming/scripts/background.sh
+++ theming/scripts/background.sh
@@ -4,6 +4,7 @@ DIR=$1
 
 cp $DIR/img/wallpaper.png $DIR/img/lockscreen.png
 convert $DIR/img/wallpaper.jpg $DIR/img/wallpaper.png
+wal -c
 wal -i $DIR/img/wallpaper.png -n --saturate 0.8
 feh --bg-fill $DIR/img/wallpaper.jpg
 
