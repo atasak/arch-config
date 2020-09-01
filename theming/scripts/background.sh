e9a840778935d0c6d7a03d50551b9a75c9576558
Niek Janssen <niek.janssen01@gmail.com>
2020-09-02 00:04:33 +0200

    Maintenance of wo  2 sep 2020  0:04:33 CEST

--- theming/scripts/background.sh
+++ theming/scripts/background.sh
@@ -6,7 +6,20 @@ cp $DIR/img/wallpaper.png $DIR/img/lockscreen.png
 convert $DIR/img/wallpaper.jpg $DIR/img/wallpaper.png
 wal -c
 wal -i $DIR/img/wallpaper.png -n --saturate 0.8
+
+echo "Setting wallpapers"
 feh --bg-fill $DIR/img/wallpaper.jpg
+
+echo "Updating Firefox colors"
 pywalfox update
 
-$DIR/telegram/telegram-palette-gen --wal
+for d in /home/niek/.config/JetBrains/*/ ; do
+    echo "Generating scheme for $d"
+    mkdir -p $d/colors
+    $DIR/intellijPywal/intellijPywalGen.sh $d
+done
+
+echo "Updating discord colors"
+$DIR/wal-discord/wal-discord
+echo "a"
+beautifuldiscord --css ~/.cache/wal-discord/style.css
