8166db9d874ae26137ba20b27489c5a8f0f0cdea
Niek Janssen <niek.janssen01@gmail.com>
2020-11-06 11:12:10 +0100

    Maintenance of vr  6 nov 2020 11:12:10 CET

--- theming/scripts/bing.js
+++ theming/scripts/bing.js
@@ -1,6 +1,15 @@
 #! /usr/bin/node
 
-var http = require('http');
+const got = require('got');
+const fs = require('fs');
 
-var options = {
-    host: 
+async function main() {
+	const response = await got('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
+	const json = JSON.parse(response.body);
+	const imgUrl = `https://bing.com/${json.images[0].url}`;
+	const img = await got(imgUrl);
+	console.log(img.body);
+	fs.writeFileSync(`${process.argv[1]}/img/wallpaper.jpg`, img.body);
+}
+
+main();
